adb47c24a182e709a3be4edd4b9bccf0215ba4b1
Bryan Chu <chu.reybr@gmail.com>
2024-06-25 01:44:52 +0800

    localStorage: another example of not needing to use useEffect()

--- src/App.jsx
+++ src/App.jsx
@@ -44,6 +44,16 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.get('selectedPlaces')) || [];
+
+    if (storedIds.indexOf(id) === -1) {
+      // -1 means nothing is found
+      localStorage.setItem(
+        'selectedPlaces',
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
